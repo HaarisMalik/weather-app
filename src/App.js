4dd54b3a1ec32ce0cc66b2fabe39839cbaab8568
HaarisMalik <haaris.malik416@gmail.com>
2022-09-12 10:47:15 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?lat=43.39&lon=79.23&exclude=current,daily&appid=016004ccad1e3aef6cb327c8ac8600ec
+  //`
+
+
   return (
     <div className="App">
      
